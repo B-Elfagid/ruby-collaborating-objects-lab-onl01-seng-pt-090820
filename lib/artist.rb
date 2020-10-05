fcc7c9f3ca98a0ba358dd319a3b27126f4e228fc
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-10-05 21:27:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,30 +23,18 @@ class Artist
    song.artist = self 
   end
 
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+    def self.find_or_create_by_name(name)
+    self.find_by_name(name) || create_by_name(name)
+  end 
+  
+  def self.find_by_name(name)
+    @@ll.detect{|a| a.name == name}
   end
+  
+  def self.create_by_name(name)
+    Artist.new(name)
+  end 
 
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
-  def save
-    @@all << self
-  end
 
   def print_songs()
     puts @songs.collect {|x| x.name}
