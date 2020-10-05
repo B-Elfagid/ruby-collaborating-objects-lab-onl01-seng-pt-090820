11f8918516e09abe4f0b5c2476bc8d6cf9ffd240
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-10-05 23:13:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,23 @@
-class Song 
-  
-  attr_accessor :name, :artist 
+class Song
+  attr_accessor :artist, :name
 
   def initialize(name)
-    @name = name 
-    end 
-    
-    def self.new_by_filename(filename)
-     splitname = filename.split(" - ")
-     song = self.new(split_name[1])
-     artist = Artist.find_or_create_by_name(split_name[0])
-     song.artist = artist
-     artist.add_song(song)
-     song
-   end 
-     
-    
+    @name = name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+end
+
   
 
   
