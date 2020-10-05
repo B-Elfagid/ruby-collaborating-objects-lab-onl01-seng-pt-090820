9f01f486812d135807faa5029144c647f9ac76a5
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-10-05 21:04:17 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize(name)
+    @song = []
+    @name = name 
+   self.save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+    
+  
+  def add_song(song)
+    self.songs << song 
+    song.artist = self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+    
+  
+end 
