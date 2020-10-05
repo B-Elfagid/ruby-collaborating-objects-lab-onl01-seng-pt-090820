bf6482167f66383d1b78c26153da7c2864b6f6ba
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-10-05 21:23:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,45 +1,51 @@
-class Artist 
-  
+class Artist
+
   attr_accessor :name, :songs
-  @@all = []
-  
-   def initialize(name)
-    @song = []
-    @name = name 
-   self.save
-  end 
-  
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || create_by_name(name)
-  end 
-  
-  def self.find_by_name(name)
-    @@ll.detect{|a| a.name == name}
+
+  @@all = [] 
+
+  def initialize(name)
+    @name = name
+    @songs = [] 
   end
-  
-  def self.create_by_name(name)
-    Artist.new(name)
-  end 
-  
-  def print_songs
-    self.songs.each do |song|
-      puts song.name 
-    end 
-  end 
-    
-    def add_song(song)
-    self.songs << song 
-    #song.artist = self 
-  end 
-  
-  def save 
-    @@all << self 
-  end 
-    
 
   def self.all
     @@all
-  end 
-    
-  
-end 
+  end
+
+  def add_song(song)
+   @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs()
+    puts @songs.collect {|x| x.name}
+  end
+
+end
+
+
